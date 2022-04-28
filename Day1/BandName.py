a949c6ba2fd2d8e203b72408ade3ede53ff46ac5
Shadi <63566383+AshenPI@users.noreply.github.com>
2022-04-29 01:35:34 +0300

    Add files via upload
    Ugly simple band name genrator

---
+++ Day1/BandName.py
@@ -0,0 +1,7 @@
+#to make a band name you take the person City name and their pit name and compine it together
+
+CityName = input("enter your city name ")
+
+pitName = input("enter your pit name ")
+
+print(pitName +" "+ CityName)
